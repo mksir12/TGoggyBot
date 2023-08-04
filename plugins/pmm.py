6f6ea3be1fa373d44a4cfbd85a7eb4cc1e47cc79
mksir12 <116490353+mksir12@users.noreply.github.com>
2023-08-04 17:05:54 +0530

    Update pmm.py

--- plugins/pmm.py
+++ plugins/pmm.py
@@ -1,11 +1,11 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-MOVIEGROUP = {ur movie group join link}
+MOVIEGROUP = {https://t.me/kmcrp_movie}
 
 @Client.on_message(filters.private | filters.text)
 async def no_pm(client, message):
 
-btn = InlineKeyboardButton("Join Here", url='https://t.me/nibrasbadusha')
+btn = InlineKeyboardButton("Join Here 💡", url=MOVIEGROUP)
 
     await.reply_message(Hey {message.from_user.mention}\n\nYou Can't get movies from here. Request on our movie group. To join click the join button below 👇, reply_markup=InlineKeyboardMarkup(btn))
