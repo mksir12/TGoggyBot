04d1817962168ebf2812249d6b3280577769e4a4
mksir12 <116490353+mksir12@users.noreply.github.com>
2023-08-04 18:33:36 +0530

    Update pmm.py

--- plugins/pmm.py
+++ plugins/pmm.py
@@ -6,5 +6,6 @@ MOVIEGROUP = {"https://t.me/kmcrp_movie"}
 @Client.on_message(filters.private | filters.text)
 async def no_pm(client, message):
 
-btn = InlineKeyboardButton("Join Here 💡", url=MOVIEGROUP)
-      await.reply_message(Hey {message.from_user.mention}\n\nYou Can't get movies from here. Request on our movie group. To join click the join button below 👇, reply_markup=InlineKeyboardMarkup(btn))
+btn = InlineKeyboardButton("Join Here", url=MOVIEGROUP)
+
+    await.reply_message(Hey {message.from_user.mention}\n\nYou Can't get movies from here. Request on our movie group. To join click the join button below 👇, reply_markup=InlineKeyboardMarkup(btn))
