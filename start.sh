278843691cbfe2d978a92d2407b76d2b7eabad7f
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2023-08-05 08:17:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mksir12/TGoggyBot.git /TGoggyBot
+  git clone git@github.com:mksir12/TGoggyBot.git /TGoggyBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /TGoggyBot
